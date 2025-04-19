571944bc8164d7b1c98c8257d6ce81f1bb47c94b
Rizqi Zulqornain <rizqizulqornain056@gmail.com>
2025-04-19 08:12:13 +0700

    Update beberapa fitur dan memperbaiki fitur

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -41,7 +41,7 @@
                 <div class="sidebar-brand-icon rotate-n-15">
                     <i class="fas fa-laugh-wink"></i>
                 </div>
-                <div class="sidebar-brand-text mx-3">SB Admin <sup>2</sup></div>
+                <div class="sidebar-brand-text mx-3">ZULQORNAIN</div>
             </a>
 
             <!-- Divider -->
@@ -336,9 +336,9 @@
                         <li class="nav-item dropdown no-arrow">
                             <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
                                 data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <span class="mr-2 d-none d-lg-inline text-gray-600 small">Douglas McGee</span>
+                                <span class="mr-2 d-none d-lg-inline text-gray-600 small">zulqornain</span>
                                 <img class="img-profile rounded-circle"
-                                    src="img/undraw_profile.svg">
+                                    src="{{ asset('sb/img/pp.jpg') }}">
                             </a>
                             <!-- Dropdown - User Information -->
                             <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
@@ -379,7 +379,7 @@
             <footer class="sticky-footer bg-white">
                 <div class="container my-auto">
                     <div class="copyright text-center my-auto">
-                        <span>Copyright &copy; Your Website 2021</span>
+                        <span>Copyright &copy; <b>RIZQI ZULQORNAIN</b></span>
                     </div>
                 </div>
             </footer>
--- routes/web.php
+++ routes/web.php
@@ -23,20 +23,24 @@ Route::get('/', [HomeController::class, 'index'])->name('home');
 
 // Buat route dengan UserController
 Route::get('/users', [UserController::class, 'index'])->name('users.index');
-
 Route::get('/users/pdf', [UserController::class, 'exportPDF'])->name('users.pdf');
 Route::get('/users/excel', function () {
     return Excel::download(new UsersExport, 'users.xlsx');
 })->name('users.excel');
 
 Route::post('/users/import', [UserController::class, 'import'])->name('users.import');
-
-Route::get('/users/{user}', [UserController::class, 'show'])->name('users.show');
-
-
-
-Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
 Route::post('/users', [UserController::class, 'store'])->name('users.store');
+Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
+Route::get('/users/{user}', [UserController::class, 'show'])->name('users.show');
 Route::get('/users/{user}/edit', [UserController::class, 'edit'])->name('users.edit');
 Route::put('/users/{user}', [UserController::class, 'update'])->name('users.update');
 Route::delete('/users/{user}', [UserController::class, 'destroy'])->name('users.destroy');
+
+
+
+
+
+
+
+
+
